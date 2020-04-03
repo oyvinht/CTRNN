859523ad3f9ea65a202337b2c862757401d5698e
Øyvin Halfdan Thuv <oyvinht@pvv.ntnu.no>
2020-04-03 15:24:29 +0200

    Starting implementation. Hiding internals. Compiling as shared library.

--- CTRNN.cpp
+++ CTRNN.cpp
@@ -1,14 +1,50 @@
 #include <iostream>
 #include "CTRNN.h"
 
-namespace ctrnn {
-
-  CTRNN::CTRNN(int netsize, double stepsize) {
+namespace ctrnn
+{
+  class CTRNN::impl
+  {
+  public:
+    void init(int netsize)
+    {
+      this->netsize = netsize;
+      activations = new float[netsize];
+      biases = new float[netsize];
+      externalCurrents = new float[netsize];
+      invTimeConstants = new float[netsize];
+      potentials = new float[netsize];
+      // Init properties of each neuron
+      for (int i = 0; i < netsize; i++)
+	{
+	  activations[i] = 0.0f;
+	  biases[i] = 0.0f;
+	  externalCurrents[i] = 0.0f;
+	  invTimeConstants[i] = 0.0f;
+	  potentials[i] = 0.0f;
+	}
+      // Init weights (netsize * netsize)
+      for (int i = 0; i < netsize; i++)
+	{
+	  for (int j = 0; j < netsize; j++)
+	    {
+	      weights[i*j] = 0.0f;
+	    }
+	}
+    }
+  private:
+    float *activations, *biases, *externalCurrents, *invTimeConstants, *potentials;
+    float *weights;
+    int netsize;
+    double stepsize;
+  };
+  CTRNN::CTRNN(int netsize, double stepsize) : pimpl{std::make_unique<impl>()}
+  {
+    pimpl->init(netsize);
     return;
   }
-  
-  CTRNN::~CTRNN() {
+  CTRNN::~CTRNN()
+  {
     return;
   }
-
 }
--- CTRNN.h
+++ CTRNN.h
@@ -1,15 +1,15 @@
 #pragma once
+#include<memory>
 
-namespace ctrnn {
-
-  class CTRNN {
-    
+namespace ctrnn
+{
+  class CTRNN
+  {
   public:
-    
     CTRNN(int netsize = 0, double stepsize = 0.01);
-    
     ~CTRNN();
-    
+  private:
+    class impl;
+    std::unique_ptr<impl> pimpl;
   };
-
 }
